9250d62cb96bd908d48f38b80b5e2e92d258ff2a
LoyBru <loyibruno@.com>
2024-10-23 22:09:37 +0200

    first commit

---
+++ dist/main.js
@@ -0,0 +1,12 @@
+"use strict";
+console.log("Hello World!");
+console.log("👨‍🔬👩‍🔬 This is my first Node Project!");
+let myVar = 3;
+myVar += 10; // 13
+console.log("myvar = ", myVar);
+let myInterface = {
+    myNumber: 10,
+    myString: 'Hello world',
+};
+console.log('myNumber = ', myInterface.myNumber);
+console.log('myString = ', myInterface.myString);
